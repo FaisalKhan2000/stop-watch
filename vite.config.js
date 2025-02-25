e94ecd58e755dc2ead58e052247b6653050ec3a5
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2025-02-25 16:43:42 +0530

    configure Vite server settings and add Docker support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Listen on all addresses
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
